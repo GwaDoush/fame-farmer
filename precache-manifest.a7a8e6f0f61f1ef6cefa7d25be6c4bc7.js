e51b5ebe51d3345daac91910da3d15456681e616
Guillaume Morel <guillaume.morel763@gmail.com>
2020-06-05 01:47:38 +0200

    Updates

---
+++ precache-manifest.a7a8e6f0f61f1ef6cefa7d25be6c4bc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf796f8f3bbf0e8b57993a2faae9f76",
+    "url": "/fame-farmer/index.html"
+  },
+  {
+    "revision": "c3eb2a6dfa623826e909",
+    "url": "/fame-farmer/static/css/main.97d18647.chunk.css"
+  },
+  {
+    "revision": "919f1b9c75aa0ff9766f",
+    "url": "/fame-farmer/static/js/2.b9f9919c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fame-farmer/static/js/2.b9f9919c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3eb2a6dfa623826e909",
+    "url": "/fame-farmer/static/js/main.ac5f7f78.chunk.js"
+  },
+  {
+    "revision": "ee962770da91afab386f",
+    "url": "/fame-farmer/static/js/runtime-main.cc5ecba3.js"
+  },
+  {
+    "revision": "cad4b3591ecdfe89ecf86ff8ebcba6e8",
+    "url": "/fame-farmer/static/media/LeagueGothic-Regular.cad4b359.otf"
+  },
+  {
+    "revision": "92adb936fd44ff5fc33fbad5e681ef69",
+    "url": "/fame-farmer/static/media/bg_player.92adb936.png"
+  }
+]);
